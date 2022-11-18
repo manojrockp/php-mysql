bc2b94fc5853566142b312ea8870ed2c119d4bc4
manojrockp <54933448+manojrockp@users.noreply.github.com>
2022-11-18 15:44:03 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 /* Database credentials. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
-define('DB_SERVER', 'database-1.ceaqyubwm5tx.ap-south-1.rds.amazonaws.com');
+define('DB_SERVER', 'database-1.crwxmfpivkwu.ap-south-1.rds.amazonaws.com');
 define('DB_USERNAME', 'admin');
 define('DB_PASSWORD', 'maNoj2000');
 define('DB_NAME', 'employees_db');
